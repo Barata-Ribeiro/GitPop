9aa005df59dfc09b85b5ebf53bcff16fd372b6b9
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-24 12:56:47 -0300

    feat: created brand logo, GitPop

---
+++ src/components/Global/BrandLogo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IoRocketSharp } from "react-icons/io5";
+
+const BrandLogo = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center p-6">
+        <IoRocketSharp
+          className="bg-orange p-[6px] rounded-ss-xl rounded-ee-xl mr-3"
+          color="white"
+          size={36}
+        />
+        <h1 className="font-rubik text-4xl">GitPop</h1>
+      </div>
+    </>
+  );
+};
+
+export default BrandLogo;
