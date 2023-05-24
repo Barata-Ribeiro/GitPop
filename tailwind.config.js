d8260038ea7c9a0c4a20e4acdd798b78b82f2f9c
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-24 12:29:13 -0300

    chore: add font rubik and open sans & colors to config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+        sans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        orange: "#e65b2e",
+        darkBlack: "#212121",
+        darkBlack2: "#333",
+        white: "#fff",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
