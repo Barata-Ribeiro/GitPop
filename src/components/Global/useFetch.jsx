5874f1f27c7ef65363337c6b8b2686fbc6302a43
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-24 15:08:02 -0300

    feat: add useFetch Hook from react course with improvements

---
+++ src/components/Global/useFetch.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  const request = React.useCallback(async (url, options = {}) => {
+    const abortController = new AbortController();
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(url, {
+        ...options,
+        signal: abortController.signal,
+      });
+      if (!response.ok) throw new Error(`Error: ${response.status}`);
+      const json = await response.json();
+      setData(json);
+      return { response, json };
+    } catch (error) {
+      if (error.name === "AbortError") {
+        console.log("Fetch cancelled");
+      } else {
+        setError(error.message);
+        return { error };
+      }
+    } finally {
+      setLoading(false);
+    }
+
+    // Cleanup function to abort fetch on component unmount
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, []);
+
+  return { data, error, loading, request };
+};
+
+export default useFetch;
