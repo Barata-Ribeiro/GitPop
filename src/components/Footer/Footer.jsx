f925f16fd2535ea2abe2b6ba4930b6f0afa45108
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-27 10:31:26 -0300

    feat: add terms modal

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import BrandLogo from "../Global/BrandLogo";
+import Terms from "./Terms";
 
 const Footer = () => {
+  const [modal, showModal] = React.useState(false);
   const currentYear = new Date().getFullYear();
+
+  const handleModal = () => {
+    showModal(!modal);
+  };
 
   return (
     <footer className="w-full sm:mt-14 mt-9 px-4 sm:px-6 lg:px-8">
@@ -12,11 +18,14 @@ const Footer = () => {
           © {currentYear} GitPop by Barata Ribeiro. All rights reserved.
         </p>
         <div className="flex items-center justify-center space-x-4 mt-8 text-sm font-semibold leading-6 text-shark-700">
-          <a href="/privacy-policy">Site Terms</a>
+          <span className="cursor-pointer" onClick={handleModal}>
+            Site Terms
+          </span>
           <div className="h-4 w-px bg-shark-500/20"></div>
-          <a href="/changelog">Repository</a>
+          <a href="https://github.com/Barata-Ribeiro/GitPop">Repository</a>
         </div>
       </div>
+      {modal && <Terms closeModal={handleModal} />}
     </footer>
   );
 };
---
+++ src/components/Footer/Terms.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { IoClose } from "react-icons/io5";
+
+const Terms = ({ closeModal }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-shark-50 text-shark-950 overflow-auto rounded-lg max-w-4xl mx-auto shadow-lg z-50 max-h-fit">
+        {/* HEADER */}
+        <div className="flex justify-between items-center p-6">
+          <h2 className="text-2xl font-bold">Terms of Service</h2>
+          <button onClick={closeModal}>
+            <IoClose size={24} />
+          </button>
+        </div>
+        <hr />
+
+        {/* TEXT */}
+        <div className="px-6 py-4">
+          <h2 className="text-lg font-bold mb-4">1. Acceptance of Terms</h2>
+          <p>
+            By accessing and using GitPop (&ldquo;Service&rdquo;), you accept
+            and agree to be bound by the terms and provision of this agreement.
+            If you do not agree to abide by these terms, please do not use the
+            Service.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4 mb-4">2. Our Service</h2>
+          <p>
+            GitPop provides a client-side application that queries the GitHub
+            API and shows the most popular repositories for a given user as
+            their profile info. This service is intended for personal and
+            non-commercial use.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4 mb-4">
+            3. Intellectual Property
+          </h2>
+          <p>
+            The GitPop brand and logo are owned by the creator. The content of
+            the Service, excluding data from the GitHub API, is the property of
+            GitPop and is protected by international copyright, trademark, and
+            other intellectual property rights.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4 mb-4">4. GitHub API Terms</h2>
+          <p>
+            GitPop uses the GitHub API in its Service. By using the Service,
+            users agree to be bound by the GitHub Terms of Service.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4 mb-4">5. Changes to Terms</h2>
+          <p>
+            GitPop reserves the right to modify these terms of service at any
+            time. It is your responsibility to regularly check for any changes.
+            If you continue to use the Service after changes are posted, you
+            agree to be legally bound by the updated terms of service.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4 mb-4">6. Disclaimer</h2>
+          <p>
+            GitPop is provided on an &ldquo;as is&rdquo; basis. GitPop disclaims
+            all warranties, whether express or implied, including without
+            limitation to the implied warranties of merchantability, fitness for
+            a particular purpose, and non-infringement. GitPop does not warrant
+            that the service will be uninterrupted, secure, or error-free.
+          </p>
+        </div>
+
+        {/* FOOTER */}
+        <hr />
+        <div className="px-6 py-4 flex justify-end">
+          <button
+            onClick={closeModal}
+            className="inline-block px-4 py-2 bg-gradient-to-r from-cinnabar-400 to-cinnabar-600 text-shark-50 font-semibold text-sm leading-snug uppercase rounded shadow-md hover:bg-gradient-to-r hover:from-cinnabar-500 hover:to-cinnabar-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gradient-to-r active:from-cinnabar-600 active:to-cinnabar-800 active:shadow-lg disabled:bg-gradient-to-r disabled:from-cinnabar-100 disabled:to-cinnabar-300 disabled:text-cinnabar-700 transition duration-150 ease-in-out"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+      <div className="fixed inset-0 bg-black opacity-50"></div>
+    </div>
+  );
+};
+
+export default Terms;
