7bcf02c2b8dfae9b3fd79d4f209630c06db733a2
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-27 12:22:15 -0300

    Added responsiveness to terms. Not perfect.

--- src/components/Footer/Terms.jsx
+++ src/components/Footer/Terms.jsx
@@ -4,18 +4,18 @@ import { IoClose } from "react-icons/io5";
 const Terms = ({ closeModal }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-shark-50 text-shark-950 overflow-auto rounded-lg max-w-4xl mx-auto shadow-lg z-50 max-h-fit">
+      <div className="bg-shark-50 text-shark-950 md:rounded-lg overflow-auto max-w-full md:max-w-4xl mx-auto shadow-lg z-50 max-h-full">
         {/* HEADER */}
-        <div className="flex justify-between items-center p-6">
-          <h2 className="text-2xl font-bold">Terms of Service</h2>
+        <div className="flex justify-between items-center p-2 sm:p-6">
+          <h2 className="text-lg sm:text-2xl font-bold">Terms of Service</h2>
           <button onClick={closeModal}>
-            <IoClose size={24} />
+            <IoClose size={20} className="sm:text-xl" />
           </button>
         </div>
         <hr />
 
         {/* TEXT */}
-        <div className="px-6 py-4">
+        <div className="p-2 sm:px-6 sm:py-4">
           <h2 className="text-lg font-bold mb-4">1. Acceptance of Terms</h2>
           <p>
             By accessing and using GitPop (&ldquo;Service&rdquo;), you accept
@@ -68,10 +68,10 @@ const Terms = ({ closeModal }) => {
 
         {/* FOOTER */}
         <hr />
-        <div className="px-6 py-4 flex justify-end">
+        <div className="px-2 py-1 sm:px-6 sm:py-4 flex sm:justify-end justify-center">
           <button
             onClick={closeModal}
-            className="inline-block px-4 py-2 bg-gradient-to-r from-cinnabar-400 to-cinnabar-600 text-shark-50 font-semibold text-sm leading-snug uppercase rounded shadow-md hover:bg-gradient-to-r hover:from-cinnabar-500 hover:to-cinnabar-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gradient-to-r active:from-cinnabar-600 active:to-cinnabar-800 active:shadow-lg disabled:bg-gradient-to-r disabled:from-cinnabar-100 disabled:to-cinnabar-300 disabled:text-cinnabar-700 transition duration-150 ease-in-out"
+            className="inline-block px-7 py-3 bg-gradient-to-r from-cinnabar-400 to-cinnabar-600 text-shark-50 font-semibold text-sm leading-snug uppercase rounded shadow-md hover:bg-gradient-to-r hover:from-cinnabar-500 hover:to-cinnabar-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gradient-to-r active:from-cinnabar-600 active:to-cinnabar-800 active:shadow-lg disabled:bg-gradient-to-r disabled:from-cinnabar-100 disabled:to-cinnabar-300 disabled:text-cinnabar-700 transition duration-150 ease-in-out"
           >
             Close
           </button>
